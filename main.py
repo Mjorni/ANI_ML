50b11ebe6972c7ee5b01a38af58101a67ab55cd5
Poko <71909795+Mjorni@users.noreply.github.com>
2024-06-02 14:54:25 +0300

    Update main.py

--- main.py
+++ main.py
@@ -4,9 +4,7 @@ from selenium import webdriver
 
 from threading import Thread
 import time
-"""
-    ФАЙЛ НЕ ИСПОЛЬЗУЕТСЯ
-"""
+
 
 headers = {
     'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
